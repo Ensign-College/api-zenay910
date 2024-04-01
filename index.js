0c9d14c5e5c89697a78193389ba11be17c91823a
Jose Yanez <yanezhjs@gmail.com>
2024-04-01 10:15:10 -0600

    endpoint

--- index.js
+++ index.js
@@ -1,67 +1,111 @@
 const Redis = require('redis');
-const { addProduct, getProduct } = require("./services/Products/ProductService.js");
 const { addOrder, getOrder } = require("./services/Orders/OrderService.js");
 const { addOrderItem, getOrderItem } = require("./services/Orders/OrderItems.js");
-const fs = require("fs");
-const Schema = JSON.parse(fs.readFileSync("./services/Orders/orderItemSchema.json", "utf8"));
-const Ajv = require("ajv");
-const ajv = new Ajv();
+const { addProduct, getProduct } = require("./services/Products/ProductService.js");
 
 const redisClient = Redis.createClient({
     url:`redis://localhost:6379`
 });
 
-exports.handler = async (event, context, callback) => {
-    try {
-        const path = event.path;
-        const method = event.httpMethod;
-        let response;
-
-        switch (path) {
-            case '/order':
-                if (method === 'POST') {
-                    const order = JSON.parse(event.body);
-                    if (!order.shippingAddress) {
-                        res.status(400).send("Missing shippingAddress in the order");
-                        return;
-                    }
-                
-                    try {
-                        await addOrder({ redisClient, newOrder: order });
-                        const orderDetails = {
-                            orderID: order.orderID,
-                            customerID: order.customerID,
-                            shippingAddress: order.shippingAddress,
-                        };
-                
-                    } catch (error) {
-                        console.error(error);
-                        res.status(500).send("Internal server error");
-                    }
-                    response = { statusCode: 200, body: JSON.stringify({ message: "Order successfully added to Redis" }) };
-                } else if (method === 'GET') {
-                    const orderID = req.params.orderID;
-    let order = await getOrder({ redisClient, orderID });
-    if (order === null) {
-        res.status(404).send("Order not found");
-    } else {
-        res.json(order);
+exports.test = async (event, context) => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({message: 'WORKS!', event, context})
     }
-                }
-                break;
-            case '/orderItems':
-                // Your orderItems POST and GET logic
-                break;
-            case '/products':
-                // Your products POST and GET logic
-                break;
-            default:
-                response = { statusCode: 404, body: JSON.stringify({ message: "Not Found" }) };
+}
+
+exports.createOrder = async (event, context) => {
+    try {
+        const order = JSON.parse(event.body);
+        // Validating properties of order
+        if (!order.shippingAddress) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ error: "Missing shippingAddress in the order" })
+            };
         }
 
-        callback(null, response);
+        await addOrder({ redisClient, newOrder: order });
+        const orderDetails = {
+            orderID: order.orderID,
+            customerID: order.customerID,
+            shippingAddress: order.shippingAddress,
+        };
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ orderDetails, message: "Order successfully added to Redis" })
+        };
     } catch (error) {
         console.error(error);
-        callback({ statusCode: 500, body: JSON.stringify({ error: "Internal server error" }) });
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: "Internal server error" })
+        };
+    }
+};
+
+exports.getOrderById = async (event, context) => {
+    try {
+        const orderID = event.pathParameters.orderID;
+        const order = await getOrder({ redisClient, orderID });
+        if (!order) {
+            return {
+                statusCode: 404,
+                body: JSON.stringify({ error: "Order not found" })
+            };
+        } else {
+            return {
+                statusCode: 200,
+                body: JSON.stringify(order)
+            };
+        }
+    } catch (error) {
+        console.error(error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: "Internal server error" })
+        };
+    }
+};
+
+exports.createProduct = async (event, context) => {
+    try {
+        const newProduct = JSON.parse(event.body);
+        await addProduct({ redisClient, newProduct });
+        return {
+            statusCode: 200,
+            body: JSON.stringify(newProduct)
+        };
+    } catch (error) {
+        console.error(error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: "Internal server error" })
+        };
+    }
+};
+
+exports.getProductById = async (event, context) => {
+    try {
+        const productID = event.pathParameters.productID;
+        const product = await getProduct({ redisClient, productID });
+        if (!product) {
+            return {
+                statusCode: 404,
+                body: JSON.stringify({ error: "Product not found" })
+            };
+        } else {
+            return {
+                statusCode: 200,
+                body: JSON.stringify(product)
+            };
+        }
+    } catch (error) {
+        console.error(error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: "Internal server error" })
+        };
     }
 };
